1405251cc1a3813cb179eb2ea1ce13cf65fc052f
YongliuLi <ylli@thoughtworks.com>
2016-07-05 15:27:24 +0800

    [#0] add exception log

--- upgrade.sh
+++ upgrade.sh
@@ -4,7 +4,7 @@ java -jar ./target/leansw-rancher-service-upgrader-1.0-SNAPSHOT.jar \
 --rancher.uri=$RANCHER_URI \
 --app.secret=$RANCHER_API_SECRET \
 --secret.key=$RANCHER_SECRET_KEY \
---environment.name="Default" \
---stack.name="deliflow-ui-and-services" \
+--environment.name="uat" \
+--stack.name="deliflow-ui-and-services-uat" \
 --service.name="deliflow-webapp"
 
